5391baec0bbe5e88563a078800d50a8269ef67d8
Andrew Roddy <andrewroddy92@yahoo.com>
2021-09-27 18:21:56 -0400

    Corrected bugs in setting color for present

--- script/script.js
+++ script/script.js
@@ -7,12 +7,12 @@ var hours = ['#9am','#10am','#11am','#12pm','#1pm',
 
 function loadFromStorage() {
     for(var i=0; i < hours.length-1; i++) {
-        $(hours[i]).children('textarea').val(localStorage.getItem(hours[i]))
+        $(hours[i]).html('textarea').val(localStorage.getItem(hours[i]))
     }
 }
 
 function storeText(id) {
-    var inputPlan = $(id).children('textarea').val()
+    var inputPlan = $(id).html('textarea').val()
     localStorage.setItem(id, inputPlan)
 }
 
@@ -44,80 +44,32 @@ $('#save5').click(function() {
     storeText('#5pm');
 })
 
-function plannerStyle() {
-    var currentTime = moment();
-    if (currentTime < moment().hour(9)) {
-        $('#9am').removeClass('present')
-        $('#9am').addClass('future')
+function kinda(currentTime, hour, id) {
+    var relTime = moment().hour(hour)
+    console.log(relTime)
+    $(id).removeClass('past present future')
+    if (currentTime.isBefore(moment().hour(hour), 'hour')) {
+        $(id).addClass('future')
     } 
-    if (currentTime > moment().hour(9)) {
-        $('#9am').removeClass('present future')
-        $('#9am').addClass('past')
+    if (currentTime.isAfter(moment().hour(hour), 'hour')) {
+        $(id).addClass('past')
     }
-    if (currentTime < moment().hour(10)) {
-        $('#10am').removeClass('present')
-        $('#10am').addClass('future')
-        } 
-    if (currentTime > moment().hour(10)) {
-        $('#10am').removeClass('present future')
-        $('#10am').addClass('past')
-        }
-    if (currentTime < moment().hour(11)) {
-        $('#11am').removeClass('present')
-        $('#11am').addClass('future')
-        } 
-    if (currentTime > moment().hour(11)) {
-        $('#11am').removeClass('present future')
-        $('#11am').addClass('past')
-        }
-    if (currentTime < moment().hour(12)) {
-        $('#12pm').removeClass('present')
-        $('#12pm').addClass('future')
-        }
-    if (currentTime > moment().hour(12)) {
-        $('#12pm').removeClass('present future')
-        $('#12pm').addClass('past')
-        }
-    if (currentTime < moment().hour(13)) {
-        $('#1pm').removeClass('present')
-        $('#1pm').addClass('future')
-        }
-    if (currentTime > moment().hour(13)) {
-        $('#1pm').removeClass('present future')
-        $('#1pm').addClass('past')
-        }
-    if (currentTime < moment().hour(14)) {
-        $('#2pm').removeClass('present')
-        $('#2pm').addClass('future')
-        }
-    if (currentTime > moment().hour(14)) {
-        $('#2pm').removeClass('present future')
-        $('#2pm').addClass('past')
-        }
-    if (currentTime < moment().hour(15)) {
-        $('#3pm').removeClass('present')
-        $('#3pm').addClass('future')
-        }
-    if (currentTime > moment().hour(15)) {
-        $('#3pm').removeClass('present future')
-        $('#3pm').addClass('past')
-        }
-    if (currentTime < moment().hour(16)) {
-        $('#4pm').removeClass('present')
-        $('#4pm').addClass('future')
-        }
-    if (currentTime > moment().hour(16)) {
-        $('#4pm').removeClass('present future')
-        $('#4pm').addClass('past')
-        }
-    if (currentTime < moment().hour(17)) {
-        $('#5pm').removeClass('present')
-        $('#5pm').addClass('future')
-        }
-    if (currentTime > moment().hour(17)) {
-        $('#5pm').removeClass('present future')
-        $('#5pm').addClass('past')
-        }
+    if (currentTime.isSame(moment().hour(hour), 'hour')) {
+        $(id).addClass('present')
+    }
+}
+
+function plannerStyle() {
+    var currentTime = moment().hour(14).minute(0).second(0);
+    kinda(currentTime, 9, '#9am');
+    kinda(currentTime, 10, '#10am');
+    kinda(currentTime, 11, '#11am');
+    kinda(currentTime, 12, '#12pm');
+    kinda(currentTime, 13, '#1pm');
+    kinda(currentTime, 14, '#2pm');
+    kinda(currentTime, 15, '#3pm');
+    kinda(currentTime, 16, '#4pm');
+    kinda(currentTime, 17, '#5pm');
 }
 
 loadFromStorage();
